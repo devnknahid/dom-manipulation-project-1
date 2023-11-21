cced1d45d1a09eee82b34ddbc81b9b1d389b16fd
devnknahid <dev.nknahid39@gmail.com>
2023-11-21 12:09:55 +0600

    area js

---
+++ area.js
@@ -0,0 +1,10 @@
+function triangel() {
+   const triangel_base_field = document.getElementById('triangel_base');
+   const triangel_base_value = triangel_base_field.value;
+   const triangel_base = parseFloat(triangel_base_value);
+
+   // triangel_height
+   const triangel_height_field = document.getElementById('triangel_height');
+   const triangel_height_value = triangel_height_field.value;
+   const triangel_height = parseFloat(triangel_height_value);
+}
