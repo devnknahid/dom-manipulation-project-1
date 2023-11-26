6e5ae2e6bf1249f0c83c70506ac253cb781e82da
devnknahid <dev.nknahid39@gmail.com>
2023-11-26 22:58:25 +0600

    fnc

--- area.js
+++ area.js
@@ -1,32 +1,70 @@
 function triangel() {
+
    const triangel_base_field = document.getElementById('triangel_base');
    const triangel_base_value = triangel_base_field.value;
    const triangel_base = parseFloat(triangel_base_value);
 
 
+   const showResult = document.getElementById('result');
+
+
    if (isNaN(triangel_base)) {
-      alert('vai base e numbre add koro');
+      showResult.innerText = 'please Input Base Number';
+      triangel_base_field.value = '';
       return;
    }
-   // triangel_base_field.value = '';
    // triangel_height
    const triangel_height_field = document.getElementById('triangel_height');
    const triangel_height_value = triangel_height_field.value;
    const triangel_height = parseFloat(triangel_height_value);
 
    if (isNaN(triangel_height)) {
-      alert('vai height e valu add koro');
+      showResult.innerText = 'please Input Height Number';
+      triangel_height_field.value = '';
       return;
    }
-   // triangel_height_field.value = '';
+
+
 
    const area = 0.5 * triangel_base * triangel_height;
+   if (area) {
+      showResult.innerText = 'Done';
+   }
    // triangel_area
    const triangel_area = document.getElementById('triangel_area');
    triangel_area.innerText = area;
+
+
+
 }
 
 // rectangel
 function rectangel() {
+   const rectangel_width_field = document.getElementById('rectangel_width');
+   const rectangel_width_value = rectangel_width_field.value;
+   const rectangel_width = parseFloat(rectangel_width_value);
+
+   if (isNaN(rectangel_width)) {
+      alert('vai width e numbre add koro');
+      return;
+   }
+   // rectangel_width_field.value = '';
+
+   // rectangel_length
+   const rectangel_length_field = document.getElementById('rectangel_length');
+   const rectangel_length_value = rectangel_length_field.value;
+   const rectangel_length = parseFloat(rectangel_length_value);
+
+   if (isNaN(rectangel_length)) {
+      alert('vai width e numbre add koro');
+      return;
+   }
+   // rectangel_length_field.value = '';
+
+   // area 
+   const area = rectangel_width * rectangel_length;
+   // rectangel_area
+   const rectangel_area = document.getElementById('rectangel_area');
+   rectangel_area.innerText = area;
 
 }
